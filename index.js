6a564725d41165c5d82fafbf7b2381880da68112
Arbaz128 <a4arbazchaudhary@gmail.com>
2024-03-16 23:49:34 +0500

    My First Repo

---
+++ index.js
@@ -0,0 +1,22 @@
+// setTimeout(()=>{
+//     let x = prompt("Enter Your Name");
+//     document.getElementById("myMarquee").innerHTML = x;
+// },4000);
+// setInterval(()=>{
+// console.log("HELLO MY NAME IS ARBAZ");
+// },4000);
+// let r = document.querySelector(".marquee");
+//     console.log(r);
+
+// function myFunction(x,y)
+// {
+//     console.log(x-y);
+// }
+// myFunction(33,77);
+
+
+// function ww(x,y)
+// {
+//     alert (x%y);
+// }
+// ww(33,77);
